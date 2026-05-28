4759a6b73d12079cf8de60041d2189acfb5cb43f
Mahmoud <mahmoud.mma.en@gmail.com>
2026-05-28 21:07:26 +0300

    Enhance Recovery Protocol State Management and Error Handling: Further optimize JSON serialization in RecoveryProtocolState and improve error handling in RecoveryGridScreen. This update aims to elevate user experience during data operations and loading states.

---
+++ brain_clean_mobile/lib/features/recovery/data/recovery_protocol_storage_provider.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'recovery_protocol_storage_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$recoveryProtocolStorageHash() =>
+    r'b8cc511d5d7036674cfa4f3e62f01406dc528935';
+
+/// See also [recoveryProtocolStorage].
+@ProviderFor(recoveryProtocolStorage)
+final recoveryProtocolStorageProvider =
+    Provider<RecoveryProtocolStorage>.internal(
+  recoveryProtocolStorage,
+  name: r'recoveryProtocolStorageProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$recoveryProtocolStorageHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef RecoveryProtocolStorageRef = ProviderRef<RecoveryProtocolStorage>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- brain_clean_mobile/lib/features/recovery/presentation/recovery_protocol_controller.g.dart
+++ brain_clean_mobile/lib/features/recovery/presentation/recovery_protocol_controller.g.dart
@@ -6,31 +6,12 @@ part of 'recovery_protocol_controller.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$recoveryProtocolDataHash() =>
-    r'38f340f0d08e6a8e37f740954c692cfd763bc3a0';
-
-/// See also [recoveryProtocolData].
-@ProviderFor(recoveryProtocolData)
-final recoveryProtocolDataProvider =
-    AutoDisposeProvider<RecoveryProtocolState>.internal(
-  recoveryProtocolData,
-  name: r'recoveryProtocolDataProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$recoveryProtocolDataHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef RecoveryProtocolDataRef = AutoDisposeProviderRef<RecoveryProtocolState>;
 String _$recoveryProtocolControllerHash() =>
-    r'ac0ee8eaa982318100f212d06e230d0597de2d86';
+    r'79fdec20729bdfa1bfb17dd1ffc2564099a1f5fe';
 
 /// See also [RecoveryProtocolController].
 @ProviderFor(RecoveryProtocolController)
-final recoveryProtocolControllerProvider = AutoDisposeNotifierProvider<
+final recoveryProtocolControllerProvider = AsyncNotifierProvider<
     RecoveryProtocolController, RecoveryProtocolState>.internal(
   RecoveryProtocolController.new,
   name: r'recoveryProtocolControllerProvider',
@@ -41,7 +22,6 @@ final recoveryProtocolControllerProvider = AutoDisposeNotifierProvider<
   allTransitiveDependencies: null,
 );
 
-typedef _$RecoveryProtocolController
-    = AutoDisposeNotifier<RecoveryProtocolState>;
+typedef _$RecoveryProtocolController = AsyncNotifier<RecoveryProtocolState>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
